1bb5685290e49f3488474bfecc6d9dff1e37778a
Ian McMahon <iankmcmahon@gmail.com>
2024-01-03 20:16:30 -0800

    Complete/Debug Step 2

--- async-await/part 2/app.js
+++ async-await/part 2/app.js
@@ -1,11 +1,28 @@
-let baseURL = "https://deckofcardsapi.com/api/deck/new/";
+let baseURL = "https://deckofcardsapi.com/api/deck";
 
 // 1.
 
 async function drawCard() {
-  res = await axios.get(`${baseURL}/draw?json`);
+  res = await axios.get(`${baseURL}/new/draw?json`);
   card = `${res.data.cards[0].value} of ${res.data.cards[0].suit}`;
   console.log(card);
 }
 
 drawCard();
+
+// 2.
+
+async function drawTwoCards() {
+  let firstCardData = await axios.get(`${baseURL}/new/draw?json`);
+  console.log(firstCardData);
+  let deckId = firstCardData.data.deck_id;
+  console.log(deckId);
+  let secondCardData = await axios.get(`${baseURL}/${deckId}/draw?json`);
+  console.log(secondCardData);
+  [firstCardData, secondCardData].forEach((card) => {
+    let { suit, value } = card.data.cards[0];
+    console.log(`${value.toLowerCase()} of ${suit.toLowerCase()}`);
+  });
+}
+
+drawTwoCards();
